d4bccbff9523e0fd2d692cf7ae06a79bad2dea1f
az3ta <24527778+az3ta@users.noreply.github.com>
2023-05-16 16:00:38 +0200

    updates

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,4 +1,17 @@
-<footer class="h-10 bg-blue-200">Footer</footer>
+<footer class="bg-blue-200">
+  <p>&#169;<?= date("Y"); ?> <?= $site->title() ?></p>
+  <ul>
+    <?php if($site->socialMediaOptions() == 'Text'): ?>
+      <?php foreach($site->socialMedia()->toStructure() as $socialItem): ?>
+        <li><a href="<?= $socialItem->url() ?>" target="_blank" rel="noopener noreferrer"><?= $socialItem->platform() ?></a></li>
+      <?php endforeach ?>
+    
+      <?php else: ?>
+        <p>do social with icons...</p>
+      <?php endif ?>
+  </ul>
+
+</footer>
 
 </body>
 </html>
