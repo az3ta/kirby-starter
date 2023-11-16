979ae7ba87ed0f675468beb520cd02eff2c83911
az3ta <24527778+az3ta@users.noreply.github.com>
2023-11-16 18:03:59 +0100

    upd: better structure + esbuild

---
+++ public/assets/js/main.js
@@ -0,0 +1,5 @@
+(() => {
+  // src/js/script.js
+  console.log("test");
+})();
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vLi4vLi4vc3JjL2pzL3NjcmlwdC5qcyJdLAogICJzb3VyY2VzQ29udGVudCI6IFsiY29uc29sZS5sb2coXCJ0ZXN0XCIpO1xuIl0sCiAgIm1hcHBpbmdzIjogIjs7QUFBQSxVQUFRLElBQUksTUFBTTsiLAogICJuYW1lcyI6IFtdCn0K
